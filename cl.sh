a96aa6b8bfe9968c88a8651ac705ca4148376b5c
C. SHEN <chunhua@me.com>
2023-12-19 16:53:54 +0800

    Tue 19 Dec 2023 16:53:54 CST

--- cl.sh
+++ cl.sh
@@ -2,11 +2,11 @@
 #
 # Author:        Chunhua Shen
 # Creation:      Wednesday 13/08/2008 12:06.
-# Last Revision: Saturday 18/05/2022 18:48.
+# Last Revision: Saturday 18/10/2023 18:48.
 
 
 
-jemdoc="./jemdoc.py"
+jemdoc="./jemdoc3.py"
 
 
 rm -f eqs/*
@@ -40,7 +40,7 @@ cd bin
 ./gcite.sh
 ./gcite_chart.sh
 
-./count_era.sh
+# ./count_era.sh
 cd ..
 
 
